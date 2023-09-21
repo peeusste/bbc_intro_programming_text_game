2c873eca732c1407fc97010b44279f129be5f816
peeusste <simonstevenson@btinternet.com>
2023-09-21 16:29:04 +0100

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,13 @@
+# sequential order of instructions, input & output, loops and conditionals
+
+while True:
+    myFirstVariable = input('What do you want to do >')
+
+    if myFirstVariable == 'help':
+        print('let me try to help you')
+    elif myFirstVariable == 'sing':
+        print('la la de da')
+    elif myFirstVariable == 'order pizza':
+        print('spicy american is on it\'s way')
+    else:
+        print('You typed - ', myFirstVariable, ' - I don\'t know what to do about that')
