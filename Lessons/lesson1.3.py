35e78ee20b01519cc7440096d236354a87eb7991
hipsterpriest <downiejim@gmail.com>
2023-10-13 13:41:13 +0100

    Update and rename lesson2.py to lesson1.3.py

---
+++ Lessons/lesson1.3.py
@@ -0,0 +1,13 @@
+# Practical exercise
+# Prompt the user to enter  their favourite pizza and favourite topping
+# Print a personalised message eg saying it's a good choice and it's your favourite
+
+# Prompt the user for their favourite pizza
+pizza = input("What's your favourite pizza? ")
+
+# Prompt the user for their favourite topping
+topping = input("And what's your favourite pizza toppin  g? ")
+
+# Print the personalized greeting
+print("Great choice!",pizza, "with", topping, "is a my favourite too!")
+
