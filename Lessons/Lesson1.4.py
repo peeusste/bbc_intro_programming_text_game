3a72fd2c4cedc9524562bd67fdc735c13baff8e4
hipsterpriest <downiejim@gmail.com>
2023-10-13 16:54:10 +0100

    Create Lesson1.4.py

---
+++ Lessons/Lesson1.4.py
@@ -0,0 +1,15 @@
+# Practical exercise
+
+# Create a countdown timer that counts down from a specified number entered by the user until it reaches zero
+# Use appropriate messaging, like at a rocket launching!
+
+# Prompt the user for a starting number
+start_number = int(input("Enter a number to start the countdown: "))
+print("Let the countdown commence!")
+# Use a while loop to count down to zero
+while start_number > 0:
+    print(start_number)
+    start_number -= 1
+
+# Print the final message after the loop
+print("We have blast off!")
