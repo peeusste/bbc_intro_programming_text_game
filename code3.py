adba5b9d995d5066763963ca862fd9e8a585495e
peeusste <simonstevenson@btinternet.com>
2023-10-16 14:22:17 +0100

    Update code3.py
    Remove string split from this lesson

--- code3.py
+++ code3.py
@@ -7,16 +7,19 @@ room_index = 0
 print('you are in', rooms[room_index])
 
 while True:
-    user_input = input('What do you want to do >').split(' ')
+    user_input = input('What do you want to do >')
 
-    if user_input[0] == 'go' or user_input[0] == 'move':
-        print('moving you', user_input[1])
+    if user_input == 'go' or user_input == 'move':
+        print('moving you to another room')
         room_index = room_index + 1
         if room_index == len(rooms):
             room_index = 0
 
         print('you are in', rooms[room_index])
 
+        # This cycles you through room after room and brings you back to the start
+
     else:
         print('sorry, I don\'t know how to help you')
+
 
