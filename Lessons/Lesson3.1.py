f254b91467c94ee2dc3dd85aa509f426ab6390b2
hipsterpriest <downiejim@gmail.com>
2023-10-16 17:52:50 +0100

    Update Lesson3.1.py
    Add extra exercise code

--- Lessons/Lesson3.1.py
+++ Lessons/Lesson3.1.py
@@ -27,3 +27,25 @@ i=0
 while i<len(films):
     print(films[i], "-", ratings[i])
     i+=1
+
+# Extra exercise (no code needed)
+
+# How would you work out what the favourite film was? 
+# Set max_rating to 0. Loop over the ratings and if the rating is greater than the current max_rating then that's the new max_rating
+# Can you see any issues and how would you resolve this?
+# If there are multiple films with the same score only one film would be stored. 
+# You could create a list of fave_films and if the score is equal to the max rating then append this to the list. If the rating is greater then set the list to be just this one film
+
+max_fave=ratings[0]
+faves=[]
+faves.append(films[0])
+i=1
+while i<len(films):
+    if ratings[i]>max_fave:
+        max_fave=ratings[i]
+        faves=films[i]
+    elif ratings[i]==max_fave:
+        faves.append(films[i])
+    i+=1
+    
+print(faves, "with a rating of", max_fave) 
