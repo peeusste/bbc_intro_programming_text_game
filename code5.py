d2ef86b861f943e64a3aa3e9b81e845c80e44dbf
peeusste <simonstevenson@btinternet.com>
2023-10-16 14:39:47 +0100

    Update code5.py
    Removed try except

--- code5.py
+++ code5.py
@@ -1,4 +1,4 @@
-# dictionaries, try except, modules and random
+# dictionaries, modules and random
 
 from random import randint
 
@@ -12,25 +12,22 @@ rooms = ['a bedroom', 'a kitchen', 'a sitting room']
 
 room_index = 0
 
-print('your name is', me['name'], 'you were born in', me['born'], 'and your age is', me['age'])
+print('your name is', me['name'], 'you were born in', me['born'], 'and your age is', me['age'],'\n')
 
 print('you are in', rooms[room_index])
 if randint(1, 3) == 1:
     print('a cold wind blows from somewhere...')
 
 while True:
-    user_input = input('What do you want to do >').split(' ')
+    user_input = input('What do you want to do >')
 
-    try:
-        if user_input[0] == 'go' or user_input[0] == 'move':
-            print('moving you', user_input[1])
-            room_index = room_index + 1
-            if room_index == len(rooms):
-                room_index = 0
+    if user_input == 'go' or user_input == 'move':
+        print('moving you to another room')
+        room_index = room_index + 1
+        if room_index == len(rooms):
+            room_index = 0
 
-            print('you are in', rooms[room_index])
+        print('you are in', rooms[room_index])
 
-        else:
-            print('sorry, I don\'t know how to help you')
-    except:
+    else:
         print('sorry, I don\'t know how to help you')
