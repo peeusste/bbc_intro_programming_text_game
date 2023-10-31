e40475dfe2db8b99adff84927dc854e435d09016
hipsterpriest <downiejim@gmail.com>
2023-10-31 16:32:19 +0000

    Create Lesson1.1.py

---
+++ Lessons/Lesson1.1.py
@@ -0,0 +1,8 @@
+# Print your favourite fruit along with a reason why
+
+print('My favourite fruit is the banana because it is yellow, tasty and fun')
+      
+# Print out a string that contains an apostrophe
+
+print("this ain't wrong")
+print('My favourite fruit is the banana because it\'s yellow, tasty and fun')
