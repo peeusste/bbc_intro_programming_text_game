705d39a1fa5e9f7c98c163db69230651369c3027
hipsterpriest <downiejim@gmail.com>
2024-01-29 11:07:12 +0000

    Update 3.1Lesson.py

--- Lessons/3.1Lesson.py
+++ Lessons/3.1Lesson.py
@@ -7,12 +7,12 @@
 films = ["Citizen Kane", "The Matrix", "The Godfather", "The Shawshank Redemption"]
 
 # Counter to run through list
-i=0
+i = 0
 
 # Loop through and print each film 
-while i<len(films):
+while i < len(films):
     print(films[i], "is one of your favourite films")    
-    i+=1
+    i += 1
 
 # Challenge #2
 # Using the same list of films, prompt the user with the name of the film and ask for a rating for that film
@@ -25,22 +25,22 @@ films = ["Citizen Kane", "The Matrix", "The Godfather", "The Shawshank Redemptio
 ratings = []
 
 # Counter to run through list
-i=0
+i = 0
 
 # Loop through each film and prompt the user for a rating
-while i<len(films):
+while i < len(films):
     print(films[i])    
     rating = int(input("Rate this film:"))
     ratings.append(rating)
-    i+=1
+    i += 1
 
 # Print out the films and their corresponding ratings
 print("Your film ratings are:")
 
 # Counter to run through list
-i=0
-while i<len(films):
+i = 0
+while i < len(films):
     print(films[i], "-", ratings[i])
-    i+=1
+    i += 1
 
 
