aef719ef9d67a8ca3be61c72a6bef7132520d56c
hipsterpriest <downiejim@gmail.com>
2026-07-28 11:25:37 +0100

    Update 1.3Lesson.py

--- Lessons/1.3Lesson.py
+++ Lessons/1.3Lesson.py
@@ -11,11 +11,22 @@ topping = input("And what's your favourite pizza topping? ")
 # Print the personalized greeting
 print("Great choice!", pizza, "with", topping, "is my favourite too!")
 
-#Extra exercise
-# Prompt the user to enter three numbers. Then, output the sum of the three numbers
+# Extra challenge
+# A pizza restaurant lets you build your own pizza.
 
-x = int(input('Enter your first number:'))
-y = int(input('Enter your second number:'))
-z = int(input('Enter your third number:'))
 
-print('The sum of those is', x+y+z)
+
+# Ask the user for the base price of a pizza
+pizza_price = float(input('What is the base price of the pizza? £'))
+
+# Ask how many extra toppings they want
+number_of_toppings = int(input('How many extra toppings would you like? '))
+
+# Each topping costs £1.50
+# Could have a variable topping_cost = 1.50 or hard-code into the final_price as below
+
+# Calculate and print the final price
+
+final_price = pizza_price + (number_of_toppings * 1.50)
+
+print('The final price is £', final_price)
